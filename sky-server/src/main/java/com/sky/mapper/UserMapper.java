05b66170d8e1b3a0d270afefe21b3dc82d7c13b0
N-uonuo <116993978+N-uonuo@users.noreply.github.com>
2023-12-03 21:04:44 +0800

    feat：用户数据统计（再看其业务逻辑）

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -2,8 +2,11 @@ package com.sky.mapper;
 
 
 import com.sky.entity.User;
+import io.swagger.models.auth.In;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.Map;
 
 
 @Mapper
@@ -22,5 +25,8 @@ public interface UserMapper{
      * @param user
      */
     void insert(User user);
+
+    //根据动态数量查询用户数量
+    Integer countByMap(Map map);
 
 }
